3c78cb8cc17c369aba6aa574c0ddea997f0581b9
tainv2002 <taivannho5a@gmail.com>
2023-05-16 15:56:15 +0700

    Handle toast axios error message

--- src/apis/auth/auth.api.ts
+++ src/apis/auth/auth.api.ts
@@ -2,7 +2,7 @@ import http from 'src/utils/http'
 import { AuthResponse } from 'src/types/auth.type'
 
 export const registerAccount = (body: { email: string; password: string }) => {
-  return http.post<AuthResponse>('register', body)
+  return http.post<AuthResponse>('registerr', body)
 }
 
 export const loginAccount = (body: { email: string; password: string }) => {
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,9 +1,43 @@
-import axios from 'axios'
+import axios, { AxiosError, HttpStatusCode } from 'axios'
+import { toast } from 'react-toastify'
 
 const http = axios.create({
   baseURL: 'https://api-ecom.duthanhduoc.com/',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 })
+
+// Add a request interceptor
+http.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// Add a response interceptor
+http.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  function (error: AxiosError) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const data: any | undefined = error.response?.data
+      const message = data?.message || error.message
+
+      toast.error(message)
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default http
